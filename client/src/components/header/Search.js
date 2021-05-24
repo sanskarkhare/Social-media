26b819b75ed106a553d20b06e27c890541edf628
sanskarkhare <sanskarkhare47@gmail.com>
2021-05-24 23:41:23 +0545

    search functionalities

--- client/src/components/header/Search.js
+++ client/src/components/header/Search.js
@@ -1,7 +1,36 @@
-import React, { useState } from 'react'
+import React, { useState, useEffect } from 'react';
+import { useSelector, useDispatch } from "react-redux";
+import { getDataApi } from '../../utils/fetchData';
+import { GLOBALTYPES } from "../../redux/actions/globalTypes";
+import { Link } from "react-router-dom";
+import UserCard from '../UserCard';
 
 const Search = () => {
     const [search, setSearch] = useState('')
+    const [users, setUsers] = useState([])
+
+    const { auth } = useSelector(state => state)
+    const dispatch = useDispatch()
+
+
+    useEffect(() => {
+        if(search && auth.token){
+            getDataApi(`search?username=${search}`, auth.token)
+            .then(res => setUsers(res.data.users))
+            .catch(err => {
+                dispatch({type: GLOBALTYPES.ALERT, payload: {error: err.response.data.msg}})
+            })
+        }
+        else{
+            setUsers([])
+        }
+    }, [search, auth.token, dispatch])
+
+    const handleClose = () => {
+        setSearch('')
+        setUsers([])
+    }
+
     return (
         <>
             <form className='search_form'>
@@ -12,7 +41,19 @@ const Search = () => {
                     <span className="material-icons">search</span>
                     <span>Search</span>
                 </div>
-                <div className="close_search">&times;</div>
+                <div className="close_search" style={{opacity: users.length === 0 ? 0 : 1}} onClick={handleClose}>
+                    &times;
+                </div>
+
+                <div className='users'>
+                    {
+                       search && users.map(user => (
+                            <Link key={user._id} to={`/profile/${user._id}`} onClick={handleClose}>
+                                <UserCard user={user} border='border'/>
+                            </Link>
+                        ))
+                    }
+                </div>
             </form>
         </>
     )
